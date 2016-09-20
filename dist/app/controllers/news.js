fc3b859ad7e63c1bf1d9001a133adb9bf7dc5c04
Ciprian Vacaru <cvacaru@sdl.com>
2016-09-20 14:19:08 +0300

    fix links

--- dist/app/controllers/news.js
+++ dist/app/controllers/news.js
@@ -7,9 +7,10 @@ module.exports.newsApi = newsApi;
 function newsApi(req, res, next) {
     servicesNews.news( (data) => {
         //res.status(200).json(data);
+        let dataString = JSON.stringify(data);
         res.render('home', {
             title: "Nunjucks",
-            data: data
+            data: dataString
         });
     });
 }
